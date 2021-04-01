0a40e1e6970c2e641e10f87b1101726ec1f3d95c
IIIIvan-Chmil <chmiliiiivan@gmail.com>
2021-04-01 19:27:24 +0300

    create: added slider not adaptive

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.swiper-container', {
+   loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.examples-button-next',
+    prevEl: '.examples-button-prev',
+  },
+});
