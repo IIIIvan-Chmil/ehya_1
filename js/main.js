6cde0ec97c0ea9671f513eaa22c93e279487da65
IIIIvan-Chmil <chmiliiiivan@gmail.com>
2021-04-06 16:13:17 +0300

    create: добавил модальное окно

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-const swiper = new Swiper('.swiper-container', {
+$(document).ready(function () {
+  const swiper = new Swiper('.swiper-container', {
    loop: true,
 
   // Navigation arrows
@@ -10,8 +11,28 @@ const swiper = new Swiper('.swiper-container', {
 
 var menuButton = document.querySelector(".header__menu-button");
 menuButton.addEventListener('click', function () {
-  console.log('Клик по кнопке меню');
   document
   .querySelector(".header__fragment")
   .classList.toggle('header__fragment--visible');
-})
+});
+
+var modalButton = $('[data-toggle=modal]');
+var closeModalButton = $(".modal__close");
+modalButton.on('click', openModal);
+closeModalButton.on('click', closeModal);
+
+function openModal() {
+  var modalOverlay = $(".modal__overlay");
+  var modalDialog = $(".modal__dialog");
+  modalOverlay.addClass('modal__overlay--visible');
+  modalDialog.addClass('modal__dialog--visible');
+}
+function closeModal(event) {
+  event.preventDefault();
+  var modalOverlay = $(".modal__overlay");
+  var modalDialog = $(".modal__dialog");
+  modalOverlay.removeClass('modal__overlay--visible');
+  modalDialog.removeClass('modal__dialog--visible');
+}
+
+});
